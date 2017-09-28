097bf71a55a768188a21d54f4654b24169f2a834
Andrei Konovalov <flotazani@mail.ru>
2017-09-28 10:38:11 +0300

    first lab var 7

---
+++ lab1v7/cmake-build-debug/CMakeFiles/lab1v7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1v7.dir/main.c.o"
+  "lab1v7.pdb"
+  "lab1v7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1v7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab1v7/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//#include "stdio.h"
+
+
+    int main() {
+    char c;
+    char str[1024];
+    printf("%s", "enater a char(ENG)\n");
+    scanf("%c", &c);
+    printf("%c",c);
+    scanf("%c", &c);
+    printf("%s","\nenter a string(ENG)\n");
+    scanf("%s",&str);
+    printf("%s",str);
+    return 0;
+}
